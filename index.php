60bc1d434aa979c9acaa8200d3c8ba60ad48d1eb
LNYou <78723629+ian20040409@users.noreply.github.com>
2023-05-04 11:05:48 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 	
 	</head>
 	<body class="is-preload">
+	
 
 		<!-- Wrapper -->
 			<div id="wrapper">
@@ -26,7 +27,11 @@
 									<ul class="icons">
 										<li><a href="https://www.facebook.com/profile.php?id=100068804133842" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
 										<li><a href="https://www.youtube.com/@ianlin17698/" class="icon brands fa-youtube alt"><span class="label">Instagram</span></a></li>
-
+										<div>
+										<h2>
+										<?php require_once("db/visitor_update.php")?>
+										</h2>
+										</div>
 									</ul>
 								</header>
 
@@ -332,7 +337,9 @@
 
 									</style>
 									<!-- Sharingbutton Facebook -->
-<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fweb.nttu.edu.tw%2Fs06%2F" target="_blank" rel="noopener" aria-label="Facebook">
+
+
+									<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fweb.nttu.edu.tw%2Fs06%2F" target="_blank" rel="noopener" aria-label="Facebook">
 	<div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--medium"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
 	  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.77 7.46H14.5v-1.9c0-.9.6-1.1 1-1.1h3V.5h-4.33C10.24.5 9.5 3.44 9.5 5.32v2.15h-3v4h3v12h5v-12h3.85l.42-4z"/></svg></div>Facebook</div>
   </a>
@@ -354,20 +361,7 @@
 	<div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--medium"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solid">
 	  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"/></svg></div>WhatsApp</div>
   </a>
-  
-
-
-
-
-
-
-
-
-
-
-
-
-									</div>
+		</div>
 
 
 
@@ -379,7 +373,15 @@
 					</div>
 
 			</div>
+<section>
+<div>
 
+
+
+
+
+</div>
+</section>
 
 
 		<!-- Scripts -->
