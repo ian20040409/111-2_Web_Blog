f971402304f1cc56b4c6e5540aa9a42754b541ce
LNYou <78723629+ian20040409@users.noreply.github.com>
2023-05-18 10:12:18 +0800

    Update trainQuery.php

--- db/trainQuery.php
+++ db/trainQuery.php
@@ -17,14 +17,16 @@ try{
     echo $e->getMessage() . "<br>\n";
 }
 */
-$sql2="select * FROM ticket";
+$sql2="select * FROM train";
 $connect->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
 $rs2=$connect->query($sql2);
 $rs2->setFetchMode(PDO::FETCH_BOTH);
 //$row3=$row2->fetch();
 while($row3=$rs2->fetch()){
-    echo  $row3['tid']."%nbsp;%nbsp;<br>";
+    echo  $row3['tid']."&nbsp;&nbsp;<br>";
     echo  $row3['uid']."<br>";
+    echo  $row3['ddate']."<br>";
+
 }
 	
 ?>
