ded636f7e4d1e958ce63853c283e39a9243f1e64
LNYou <78723629+ian20040409@users.noreply.github.com>
2023-05-27 18:16:49 +0800

    php

--- db/comment.php
+++ db/comment.php
@@ -8,6 +8,12 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
+// 檢查是否已設定表單提交的 cookie
+if (isset($_COOKIE['form_submitted'])) {
+    echo "⚠️ 錯誤：您已經提交過表單，請勿重複提交。";
+    exit;
+}
+
 try {
     $sql = "INSERT INTO comment (name, email, message) VALUES (:name, :email, :message)";
     $stmt = $connect->prepare($sql);
@@ -18,18 +24,20 @@ try {
     $result = $stmt->execute();
 
     if ($result === false) {
-        $msg = "更新失败。<br>\n";
+        $msg = "更新失敗。<br>\n";
     } else {
         $msg = "更新成功。<br>\n";
     }
 
     echo $msg;
 } catch (PDOException $e) {
-    
-    echo "⚠️錯誤：" . $e->getMessage();
+    echo "⚠️ 錯誤：" . $e->getMessage();
 }
 
-echo "👤 ID：" . $name . "<br>";
-echo "💬 留言内容：" . $message . "<br>";
+echo "👤 顯示名子稱：" . $name . "<br>";
+echo "💬 留言內容：" . $message . "<br>";
+
+// 設定一個 cookie 來標記表單已提交
+setcookie('form_submitted', '1', time() + 3600); // 在此示例中，cookie 有效期設定為 1 小時
 
 ?>
--- db/feedback.php
+++ db/feedback.php
@@ -4,25 +4,28 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 include_once("conn.php");
 
+// 檢查是否已經提交過表單
+if (isset($_COOKIE['form_submitted'])) {
+    // 表單已經提交過，執行相應的處理（例如顯示錯誤訊息）
+    echo "您已經提交過表單，請勿重複提交。";
+    // 可以進行重導向或顯示相應的錯誤訊息等
+    exit;
+}
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-
 try {
-    $sql = "INSERT INTO feedback (name, email, message) VALUES ('$name', '$email', '$message')";
-    
-    echo $sql . "<br>\n";
+    // 處理表單提交
+    // ...
 
-    $result = $connect->exec($sql);
+    // 表單處理完畢，將設置一個 cookie 來標記表單已提交
+    setcookie('form_submitted', '1', time() + 3600); // 在此示例中，cookie 有效期設定為 1 小時
 
-    if ($result === false) {
-        $msg = "fail update. <br>\n";
-    } else {
-        $msg = "success update. <br>\n";
-    }
-
-    echo $msg;
+    // 可以進行重導向或顯示成功訊息等
+    header("Location: feedbackok.php"); // 將用戶重導向到成功頁面
+    exit;
 } catch (PDOException $e) {
     echo $e->getMessage() . "<br>\n";
 }
