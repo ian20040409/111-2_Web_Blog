97cb0ad1e3a686ee3e5f4fe9a3feaa216683b9ac
LNYou <78723629+ian20040409@users.noreply.github.com>
2023-06-06 17:25:57 +0000

    Update comment.php

--- db/comment.php
+++ db/comment.php
@@ -4,12 +4,14 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 
 include_once("conn.php");
 
+session_start();
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-// 檢查是否已設定表單提交的 cookie
-if (isset($_COOKIE['form_submitted'])) {
+// 檢查是否已設定表單提交的 session 變數
+if (isset($_SESSION['form_submitted'])) {
     echo "⚠️ 錯誤：您已經提交過表單，請勿重複提交。";
     exit;
 }
@@ -34,10 +36,10 @@ try {
     echo "⚠️ 錯誤：" . $e->getMessage();
 }
 
-echo "👤 顯示名子稱：" . $name . "<br>";
+echo "👤 顯示名稱：" . $name . "<br>";
 echo "💬 留言內容：" . $message . "<br>";
 
-// 設定一個 cookie 來標記表單已提交
-setcookie('form_submitted', '1', time() + 3600); // 在此示例中，cookie 有效期設定為 1 小時
+// 設定一個 session 變數來標記表單已提交
+$_SESSION['form_submitted'] = true;
 
 ?>
